abe81c655c46d991389096917717110263564137
thomasklz <pipos_rgt@hotmail.com>
2020-07-06 18:57:39 -0500

    validar usuario

--- src/app/pages/validar-usuario/validar-usuario.page.ts
+++ src/app/pages/validar-usuario/validar-usuario.page.ts
@@ -1,131 +1,69 @@
-import { Component, OnInit,AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { Router } from '@angular/router';
 import { MenuController, ToastController } from '@ionic/angular';
 import { Network } from "@ionic-native/network/ngx";
-
 @Component({
   selector: 'app-validar-usuario',
   templateUrl: './validar-usuario.page.html',
   styleUrls: ['./validar-usuario.page.scss'],
 })
-export class ValidarUsuarioPage implements OnInit,AfterViewInit {
-
-  formValidarCorreo : FormGroup;
-
+export class ValidarUsuarioPage implements OnInit, AfterViewInit {
+  formValidarCorreo: FormGroup;
   constructor(
     private usuarioService: UsuarioService,
     private router: Router,
-    private menuController:MenuController,
-    private network:Network,
+    private menuController: MenuController,
+    private network: Network,
     private toastController: ToastController
   ) {
-
     this.formValidarCorreo = new FormGroup({
-      _usuario  : new FormControl('',[Validators.required,Validators.email])
+      _usuario: new FormControl('', [Validators.required, Validators.email])
     });
-
-   }
-
+  }
   ngOnInit() {
-
-
-    
   }
-  ngAfterViewInit(){
-    console.log("hola");
-    
-    // watch network for a disconnection
-    let disconnectSubscription = this.network.onDisconnect().subscribe(() => {
-      console.log('network was disconnected :-(');
-    });
+  ngAfterViewInit() {
 
-    // stop disconnect watch
-    disconnectSubscription.unsubscribe();
-
-
-    // watch network for a connection
-    let connectSubscription = this.network.onConnect().subscribe(() => {
-      console.log('network connected!');
-      // We just got a connection but we need to wait briefly
-      // before we determine the connection type. Might need to wait.
-      // prior to doing any api requests as well.
-      setTimeout(() => {
-        if (this.network.type === 'wifi') {
-          console.log('we got a wifi connection, woohoo!');
-        }
-      }, 3000);
-    });
-
-    // stop connect watch
-    connectSubscription.unsubscribe();
   }
-  
-  ionViewWillEnter(){
+  ionViewWillEnter() {
     this.menuController.enable(false);
-    // this.menuController.
   }
-  
-  
-  _validarformValidarCorreo(){
-    debugger
-    if (this.formValidarCorreo.valid==true) {
+  _validarformValidarCorreo() {
+    if (this.formValidarCorreo.valid == true) {
       this._validarUsuario();
     }
   }
-
-  _validarUsuario(){
-    debugger
+  _validarUsuario() {
     this.usuarioService._validarCorreo(this.formValidarCorreo.get('_usuario').value)
-      .then(data=>{
-        if (data['http']['codigo']=='200') {
-          console.log(data['respuesta']);
-          localStorage.setItem("_correo",data['respuesta']);
+      .then(data => {
+        if (data['http']['codigo'] == '200') {
+          localStorage.setItem("_correo", data['respuesta']);
           this.router.navigateByUrl("login");
-          // this.ocultar = false;
         } else {
-          console.log("no user");
-          this.presentToastWithButtons("El usuario no existe, por favor, verifique el usuario",3000 );
+          this.Toast("Correo no valido", 3000);
         }
-      }).catch(error=>{
-
-      }).finally(()=>{
-
-      });
+      }).catch(error => {
+        this.Toast("Revise su conexión", 3000);
+      })
   }
-
-  async presentToastWithButtons(_mensaje:string,_duracion:number=2000) {
+  async Toast(_mensaje: string, _duracion: number = 2000) {
     const toast = await this.toastController.create({
-      animated: true,
-      buttons: [
-        // {
-        //   side: 'start',
-        //   icon: 'star',
-        //   text: 'Favorite',
-        //   handler: () => {
-        //     console.log('Favorite clicked');
-        //   }
-        // },
-        {
-          text: 'Cerrar',
-          role: 'cancel',
-          handler: () => {
-            // console.log('Cancel clicked');
-          }
-        }
-      ],
-      // color: 'primary',
-      // cssClass: 'toast-success',
-      duration: _duracion,
-      // header: 'Toast header',
-      keyboardClose: true,
       message: _mensaje,
-      // mode: 'ios',
       position: 'bottom',
-      translucent: true
+      animated: true,
+      duration: _duracion,
+      color: 'dark',
+      cssClass: 'toastFormato',
+      buttons: [
+        {
+          side: 'end',
+          icon: 'close-circle-outline',
+          role: 'cancel',
+        }
+      ]
     });
     toast.present();
   }
-
 }
