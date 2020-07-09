4ef72111d8f01ede1c1540dd94d4e17145ff890f
thomasklz <pipos_rgt@hotmail.com>
2020-07-08 21:06:51 -0500

    home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
-
+import { AsignarEncuestadoService } from 'src/app/services/asignar-encuestado.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+numeroCuestionarioNuevos:number;
+numeroCuestionarioCanceladas:number=0;
   constructor(
-    private menuController:MenuController
+    private menuController:MenuController,
+    private asignarEncuestadoService:AsignarEncuestadoService
   ) { }
 
   ngOnInit() {
     this.menuController.enable(true);
+    this._consultarCuestionariosNuevos();
+  }
+  _consultarCuestionariosNuevos(){
+    this.asignarEncuestadoService._consultarporidasignarusuariotipousuariotecnico(localStorage.getItem('IdAsignarUsuarioTipoUsuarioEncriptado')) 
+      .then(data=>{
+        this.numeroCuestionarioNuevos=data["respuesta"].length;
+        data["respuesta"].forEach(element => {
+          if(element.Estado==0){
+            this.numeroCuestionarioCanceladas++
+          }
+       });
+      }).catch(error=>{
+        debugger
+      })
   }
 
 }
--- src/app/services/asignar-encuestado.service.ts
+++ src/app/services/asignar-encuestado.service.ts
@@ -24,7 +24,6 @@ export class AsignarEncuestadoService {
 
   _consultarporidasignarusuariotipousuariotecnico(_idAsignarUsuarioTipoUsuarioTecnicoEncriptado){
     const _body = new HttpParams();
-    // debugger
     return new Promise((resolve, reject) => {
       this.http.post(url+'asignarencuestado_consultarporidasignarusuariotipousuariotecnico?_idAsignarUsuarioTipoUsuarioTecnicoEncriptado='+_idAsignarUsuarioTipoUsuarioTecnicoEncriptado,_body.toString(),{headers:this._header})
                 .subscribe(res=>{
@@ -34,5 +33,14 @@ export class AsignarEncuestadoService {
                 });
     });
   }
-
+  mostrarEncuestasPorTecnico(_idTipoUsuarioTecnico){
+   return new Promise((resolve, reject) => {
+    this.http.get(url+'cuestionarios/nuevos/tecnico?_idUsuarioTipoUsuarioTecnico='+_idTipoUsuarioTecnico)
+                .subscribe(res=>{
+                  resolve(res);
+                },(err)=>{
+                  reject(err);
+                });
+    });
+  }
 }
